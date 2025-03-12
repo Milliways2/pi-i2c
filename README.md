b47f42455b546b28b7d503b52b3d7ed045827b57
Ian Binnie <ibinnie@binnie.id.au>
2025-03-12 17:08:20 +1100

    Python overlay
    Added Python overlay

--- README.md
+++ README.md
@@ -5,5 +5,20 @@ pi-i2c is an I²C C library for Raspberry Pi using the I²C kernel driver.
 It includes GPIO access routines for MCP23017 16-Bit I/O Expander.  
 Allows access by individual *pin*, *port* (8 bit) or *device* (16 bit)
 
+Overlays
+========
+Includes python overlay.
+This is available with `include pi_i2c`.
+
+Documentation
+=============
+To read documentation:-  
+	C:	`man pi-i2c` & `man MCP23017`  
+	Python:	`python3 -m pydoc pi_i2c`  
+
+Extensions
+==========
+GPIO access routines for MCP23017 16-Bit I/O Expander (requires MCP23017.h)
+
 *I initially developed this as part of a larger project, but realised it could form a stand alone library.  
 This should work on any Linux system which includes the I²C kernel driver.*
---
+++ source/DS3231.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+# 2025-03-11
+
+from pi_i2c import *
+import sys
+
+"""
+Display status of DS3231 RTC - assumes time in UTC 24 hour mode
+"""
+
+i2cBus = 1
+i2cAddr = 0x68
+
+def bcdToDec(b):
+    return (b // 16) * 10 + (b % 16)
+
+# The time and calendar registers are BCD
+SECONDS = 0x00
+MINUTES = 0x01
+HOURS = 0x02
+DAY = 0x03
+DATE = 0x04
+MONTH = 0x05
+YEAR = 0x06
+A1M1 = 0x07
+A1M2 = 0x08
+A1M3 = 0x09
+A1M4 = 0x0A
+A2M2 = 0x0B
+A2M3 = 0x0C
+A2M4 = 0x0D
+CONTROL = 0x0E
+TEMP = 0x11
+TEMPFRAC = 0x12
+
+handle = i2cOpen(i2cBus, i2cAddr)
+if (handle < 0):
+    sys.exit(handle) # Note return DOES NOT guarantee device exists at address!
+
+secs = i2cRead8(handle, SECONDS)    # Detect device exists at address
+if (secs < 0):
+    print("No device at address {:02x}".format(i2cAddr))
+    sys.exit(-1)
+secs = bcdToDec(secs)
+mins = bcdToDec(i2cRead8(handle, MINUTES))
+hours = bcdToDec(i2cRead8(handle, HOURS))
+days = bcdToDec(i2cRead8(handle, DATE))
+months = bcdToDec(i2cRead8(handle, MONTH))
+years = bcdToDec(i2cRead8(handle, YEAR))
+
+print("The RTC time is  {:2d}:{:02d}:{:02d}".format(hours, mins, secs))
+print("The RTC date is  {:2d}-{:02d}-{:02d}".format(years, months, days))
+
+temperature = i2cRead8(handle, TEMP) + ((i2cRead8(handle, TEMPFRAC) >> 6) * 0.25)
+print("The temperature is  {}°C".format(temperature))
